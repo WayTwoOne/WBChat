de735688c1582d4b2687be9a23706438394af8d6
WayTwoOne <vladimirzakharovj@yandex.ru>
2024-07-12 08:59:08 -0700

    HW#2

---
+++ WBChat/HW2/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  WBMessenger
+//
+//  Created by Vladimir on 04.06.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        WalkthroughView()
+    }
+}
+
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WBChat/Views/Walkthrough/UserAgreementAndButtonView.swift
@@ -0,0 +1,50 @@
+//
+//  UserAgreementAndButtonView.swift
+//  WBMessenger
+//
+//  Created by Vladimir on 04.06.2024.
+//
+
+import SwiftUI
+
+struct UserAgreementAndButtonView: View {
+    @State var isPresented = false
+    @Environment(\.colorScheme) var colorScheme
+    
+    let width = UIScreen.main.bounds.width
+    let height = UIScreen.main.bounds.height
+    
+    var body: some View {
+        VStack() {
+            Spacer()
+            Text("Пользовательское соглашение")
+                .font(.system(size: 14))
+                .bold()
+                .padding()
+            Button {
+                ()
+            } label: {
+                Text("Начать общаться")
+                    .frame(width: width * 0.85, height: height * 0.07)
+                    .cornerRadius(30)
+                    .background(Color(red: RGBColor(154), green: RGBColor(65), blue: RGBColor(254)))
+                    .foregroundColor(.white)
+                    .cornerRadius(30)
+                    .font(.system(size: 16))
+                    .bold()
+            }
+        }
+    }
+}
+
+extension UserAgreementAndButtonView {
+    func RGBColor(_ color: Double) -> Double {
+        color / 255
+    }
+}
+
+struct UserAgreementAndButtonView_Previews: PreviewProvider {
+    static var previews: some View {
+        UserAgreementAndButtonView()
+    }
+}
