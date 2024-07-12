3e7e0ffabc8a9a2dad87af6bb0c60f68a2781a08
WayTwoOne <vladimirzakharovj@yandex.ru>
2024-07-12 08:55:31 -0700

    Initial Commit

---
+++ WBChat/WBChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  WBChatApp.swift
+//  WBChat
+//
+//  Created by Vladimir on 12.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct WBChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
