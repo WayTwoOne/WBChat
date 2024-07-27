137b606e6f969ba2489e43b8277b840388a9897c
WayTwoOne <vladimirzakharovj@yandex.ru>
2024-07-27 09:49:53 -0700

    Created new branch

---
+++ WBChat/Contacts/DispatchImage/Image.swift
@@ -0,0 +1,15 @@
+//
+//  Image.swift
+//  WBChat
+//
+//  Created by Vladimir on 27.07.2024.
+//
+
+import SwiftUI
+
+class DispatchImage: ObservableObject {
+    func asyncImage(frome name: String) -> Image {
+        
+        return Image(name)
+    }
+}
